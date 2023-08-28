b61f0cd26d7502933511e456c882f6d96007e9c0
dohee <wlehgml94@naver.com>
2023-08-28 18:49:46 +0900

    230828 :: 코드잇 리액트 칼로리

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
-import ReviewList from "./ReviewList";
+import React, { useState } from "react";
+import FoodList from "./FoodList";
 import items from "../mock.json";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+
+  const handleNewestClick = () => setOrder("createdAt");
+
+  const handleCalorieClick = () => setOrder("calorie");
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
   return (
     <div>
-      <ReviewList items={items} />
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} />
     </div>
   );
 }
+
 export default App;
